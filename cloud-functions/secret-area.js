8c7677bb4385c6e2c4ec9c64ee42a16f4fe08aff
Jim <JimFuhrman@Yahoo.com>
2021-04-19 08:00:29 -0500

    Updated cloud function to include space between ifs and the conditions

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,18 +1,18 @@
 exports.handler = function(event, context, callback) {
     const secretContent = `
-    <h3>Welcome to the secret area </h3>
-    <p>Here we can tell you that the sky is <strong>blue</strong>, and 2+2=4</p>
+    <h3>Welcome to the secret area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four</p>
     `
 
     let body
 
-    if(event.body) {
+    if (event.body) {
         body = JSON.parse(event.body)
     } else {
         body = {}
     }
 
-    if(body.password == "javascript") {
+    if (body.password == "javascript") {
         callback(null, {
             statusCode: 200,
             body: secretContent
