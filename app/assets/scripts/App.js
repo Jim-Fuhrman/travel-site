2b487837a7fa8090894f4a4f8dd945823c00c6da
Jim <JimFuhrman@Yahoo.com>
2021-06-11 07:24:43 -0500

    removing primary-nav--pull-right references

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,31 +1,45 @@
-import '../styles/styles.css';
-import 'lazysizes'
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll'
-import StickyHeader from './modules/StickyHeader'
-import ClientArea from './modules/ClientArea'
+import "../styles/styles.css";
+import "lazysizes";
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
+import ClientArea from "./modules/ClientArea";
 
-new ClientArea()
-new StickyHeader()
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+//React Related Code Goes Here
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import react components that we created
+import MyAmazingComponent from "./modules/MyAmazingComponent";
+
+ReactDOM.render(
+  <MyAmazingComponent />,
+  document.querySelector("#my-react-example")
+);
+
+new ClientArea();
+new StickyHeader();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 new MobileMenu();
-let modal
+let modal;
 
-document.querySelectorAll(".open-modal").forEach(el => {
-    el.addEventListener("click", e => {
-        e.preventDefault()
-       if(typeof modal == "undefined") {
-            import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
-                modal = new x.default()
-                setTimeout(() => modal.openTheModal(), 20)
-            }).catch(() => console.log("there was a problem"))
-       } else {
-            modal.openTheModal()
-       }
-    })
-})
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("there was a problem"));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
 
-if(module.hot) {
-    module.hot.accept()
+if (module.hot) {
+  module.hot.accept();
 }
